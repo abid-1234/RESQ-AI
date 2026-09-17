557a9e3494a978c6d0ad61e703d9a2e7693a758a
Shaik Abid <abidshaik712@gmail.com>
2026-09-17 23:48:41 +0530

    Initial commit

--- frontend/dist/assets/indiaHazardSeeds-_QuQIdoi.js
+++ frontend/dist/assets/indiaHazardSeeds-_QuQIdoi.js
@@ -1,4 +1,4 @@
-import{c as e}from"./index-iUtrxKgF.js";/**
+import{c as e}from"./index-Cd-781t8.js";/**
  * @license lucide-react v0.546.0 - ISC
  *
  * This source code is licensed under the ISC license.
